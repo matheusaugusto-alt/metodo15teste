e858b84fadd990062b7ce26dfb25ad043cc54961
Segredo Das Redes <segredodasredes@gmail.com>
2025-09-29 12:33:38 +0000

    altere o texto para "Seu passo a passo para deixar o cachorro tranquilo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
               Método FICA-15
             </h1>
             <p className="mt-4 text-lg md:text-xl text-muted-foreground max-w-xl mx-auto lg:mx-0">
-              Seu passo a passo para um banho com cachorro tranquilo.
+              Seu passo a passo para deixar o cachorro tranquilo para o banho
             </p>
           </div>
           <div className="relative h-80 lg:h-full min-h-[300px] rounded-3xl overflow-hidden shadow-lg shadow-primary/10">
